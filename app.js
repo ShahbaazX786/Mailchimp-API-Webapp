27e70f3404aa0cdc2dafdd00eb6356262f0d535c
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-09-16 12:01:10 +0530

    added post method, and added form attributes, tweaked styles.

--- app.js
+++ app.js
@@ -6,10 +6,20 @@ const request = require('request');
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static("public")); // used to link and load the static files like styles, images , icons etc via the express server
+
+app.use(bodyParser.urlencoded({extended:true})); // used to parse the input info received in the form of post request. 
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/signup.html');
+})
+
+app.post('/',(req,res)=>{
+    const body=req.body;
+    const firstName=body.fName;
+    const lastName=body.lName;
+    const email=body.email;
+    console.log(firstName,lastName,email);
 })
 
 
