1def2ce7d63a69d96b2258586a3eb0364b9ea25f
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-09-16 11:17:05 +0530

    created a public folder read the comment #3325

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const request = require('request');
 
 const app = express();
 
+app.use(express.static("public"));
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/signup.html');
+})
+
+
 app.listen(3000, ()=>{
     console.log("server started at port 3000 broo!!");
 })
